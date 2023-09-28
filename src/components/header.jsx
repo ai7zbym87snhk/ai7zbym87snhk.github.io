3bc804fde3ed65bd21309c84bf0c33f29051daaa
ai7zbym87snhk <146280613+ai7zbym87snhk@users.noreply.github.com>
2023-09-28 12:06:27 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ const header = () => {
     // For IE logo and all including the name of the puzzle
     return (
         <Container maxW='xl' centerContent>
-            <a href="https://ie.nitk.ac.in/" target='blank'><img src="src/assets/logo.png" alt="Website Logo" style={{ width: '100px', height: '100px', marginRight: '1400px',marginTop:'15px'}} /></a>
+            <a href="https://ie.nitk.ac.in/" target='blank'><img src="https://ie.nitk.ac.in/static/img/ielogo-transparent.png" alt="Website Logo" style={{ width: '100px', height: '100px', marginRight: '1400px',marginTop:'15px'}} /></a>
             <Text color="white" marginTop="-50px" fontSize="50px">Support is all you need</Text>
         </Container >
 
